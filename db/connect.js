9bdd023b7c6c7fb743a82cea449c5995bb520ed3
Anthony Herrera <ankehecr100@gmail.com>
2021-10-19 12:37:33 -0500

    configuration to database

---
+++ db/connect.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const connectDB = async (url) => {
+    await mongoose
+        .connect(url)
+        .then(() => console.log("Connected to DB..."))
+        .catch((err) => console.log(err));
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -3,18 +3,24 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+const connectDB = require("./db/connect");
+
 const port = process.env.PORT || 3000;
 
 app.set("trust proxy", 1);
 
-app.use(express.static("public"));
+app.use(cors());
+
+//app.use(express.static("public"));
+app.use("/public", express.static(process.cwd() + "/public"));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
 
-const start = () => {
+const start = async () => {
     try {
+        await connectDB(process.env.MONGO_URI);
         app.listen(port, () =>
             console.log(`Server is listening on port ${port}...`)
         );
